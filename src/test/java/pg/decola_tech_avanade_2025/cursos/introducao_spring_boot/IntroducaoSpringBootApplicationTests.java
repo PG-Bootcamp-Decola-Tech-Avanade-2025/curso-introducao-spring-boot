ecd0964f6f7562506a81877ab352f0679867dfb7
Pedro Ghiotti <pedro.ghiotti.m@gmail.com>
2025-03-04 14:10:50 -0300

    chore: adicionada base do projeto com dependências iniciais

---
+++ src/test/java/pg/decola_tech_avanade_2025/cursos/introducao_spring_boot/IntroducaoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package pg.decola_tech_avanade_2025.cursos.introducao_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroducaoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
